594d386bfbbdbce06eb8af51637997be58a31767
masi846 <masii.shaneshin@gmail.com>
2020-11-23 22:55:23 -0500

    AssertEquall

---
+++ assertEqual.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+
+if (actual != expected) {
+
+  return false;}
+
+  
+
+    return true
+
+  
+
+
+
+};
+
+// TEST CODE
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+assertEqual(1, 1);
